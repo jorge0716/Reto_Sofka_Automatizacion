29e7b7bd3cfc825777a4bdd6011c92cba4fedc92
Jorge_Agudelo <williamchehezy>
2021-09-25 17:36:33 -0500

    Se implememnta la clase de las preguntas del tercer y cuarto nivel

--- src/main/java/Reto_Sofka_Trainning/nivelCuatro.java
+++ src/main/java/Reto_Sofka_Trainning/nivelCuatro.java
@@ -1,5 +1,58 @@
 package Reto_Sofka_Trainning;
 
 public class nivelCuatro {
+	public void Pregunta1()
+    {
+		System.out.println("Premio del cuarto nivel: 800.000 pesos");
+        System.out.println("¿El pintor de la ultima cena fue ?");
+        System.out.println("1: Pablo Picasso ");
+        System.out.println("2: Vincent van Gogh ");
+        System.out.println("3: Claude Monet");
+        System.out.println("4: Leonardo Da Vinci ");
+
+
+
+    };
+    public void Pregunta2()
+    {
+    	System.out.println("Premio del cuarto nivel: 800.000 pesos");
+        System.out.println("¿Cual era la moneda española antes del euro?");
+        System.out.println("1: Peseta");
+        System.out.println("2: Peso");
+        System.out.println("3: Lira ");
+        System.out.println("4: Libra ");
+
+    };
+
+    public void Pregunta3()
+    {
+    	System.out.println("Premio del cuarto nivel: 800.000 pesos");
+        System.out.println("¿Quien fue la pareja de sanson ?");
+        System.out.println("1: Artemisa ");
+        System.out.println("2: Dalila ");
+        System.out.println("3: Ada");
+        System.out.println("4: Aida ");
+
+    };
+    public void Pregunta4()
+    {
+        System.out.println("Premio del cuarto nivel: 800.000 pesos");
+        System.out.println("¿La capital de ucrania es ?");
+        System.out.println("1: Kiev ");
+        System.out.println("2: Moldavia ");
+        System.out.println("3: Luxemburgo");
+        System.out.println("4: Varsovia ");
+
+    };
+    public void Pregunta5()
+    {
+    	System.out.println("Premio del cuarto nivel: 800.000 pesos");
+        System.out.println("¿En que año se firmo se pacto de Varsovia ?");
+        System.out.println("1: 1953");
+        System.out.println("2: 1955");
+        System.out.println("3: 1954");
+        System.out.println("4: 1956 ");
+
+    };
 
 }
--- src/main/java/Reto_Sofka_Trainning/nivelTres.java
+++ src/main/java/Reto_Sofka_Trainning/nivelTres.java
@@ -1,5 +1,59 @@
 package Reto_Sofka_Trainning;
 
 public class nivelTres {
+	
+	 public void Pregunta1()
+	    {
+	        System.out.println("Premio del tercer nivel: 500.000 pesos");
+	        System.out.println("¿En que país se encuentra la torre pisa?");
+	        System.out.println("1: Italia ");
+	        System.out.println("2: Colombia ");
+	        System.out.println("3: Austria");
+	        System.out.println("4: Japon ");
+
+
+
+	    };
+	    public void Pregunta2()
+	    {
+	        System.out.println("Premio del tercer nivel: 500.000 pesos");
+	        System.out.println("¿ Cual es la capital de alemania?");
+	        System.out.println("1: Amanzonas");
+	        System.out.println("2: Caracas ");
+	        System.out.println("3: Bogota ");
+	        System.out.println("4: Berlin");
+
+	    };
+
+	    public void Pregunta3()
+	    {
+	        System.out.println("Premio del tercer nivel: 500.000 pesos");
+	        System.out.println("¿Cual es la capital de rusia");
+	        System.out.println("1: Moscu");
+	        System.out.println("2: Otawa");
+	        System.out.println("3: Medellin ");
+	        System.out.println("4: cali ");
+
+	    };
+	    public void Pregunta4()
+	    {
+	        System.out.println("Premio del tercer nivel: 500.000 pesos");
+	        System.out.println("¿ Que ciudad de colombia es conocida con la ciudad de los parques?");
+	        System.out.println("1: Valledupar ");
+	        System.out.println("2: Cartagena ");
+	        System.out.println("3: Pasto");
+	        System.out.println("4: Bucaramanga ");
+
+	    };
+	    public void Pregunta5()
+	    {
+	        System.out.println("Premio del tercer nivel: 500.000 pesos");
+	        System.out.println("¿Cuantos dias tiene la semana?");
+	        System.out.println("1: 7");
+	        System.out.println("2: 4");
+	        System.out.println("3: 12");
+	        System.out.println("4: 10 ");
+
+	    };
 
 }
