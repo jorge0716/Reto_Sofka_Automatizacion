71e7925be10b58afe7e0e2eb1224637ea6641758
Jorge_Agudelo <williamchehezy>
2021-09-25 17:44:32 -0500

    Se implememnta la clase de las preguntas del quinto nivel

--- src/main/java/Reto_Sofka_Trainning/nivelCinco.java
+++ src/main/java/Reto_Sofka_Trainning/nivelCinco.java
@@ -1,5 +1,59 @@
 package Reto_Sofka_Trainning;
 
 public class nivelCinco {
+	
+	  public void Pregunta1()
+	    {
+	        System.out.println("Premio del quinto nivel: 900.000 pesos");
+	        System.out.println("¿Alexander Fleming descubrió?");
+	        System.out.println("1: El boro ");
+	        System.out.println("2: La penicilina ");
+	        System.out.println("3: El azufre");
+	        System.out.println("4: El radio  ");
+
+
+
+	    };
+	    public void Pregunta2()
+	    {
+	        System.out.println("Premio del quinto nivel: 900.000 pesos");
+	        System.out.println("¿ Jimmy carter fue el presidente de estados unidos No.?");
+	        System.out.println("1: 40");
+	        System.out.println("2: 39 ");
+	        System.out.println("3: 50 ");
+	        System.out.println("4: 12");
+
+	    };
+
+	    public void Pregunta3()
+	    {
+	        System.out.println("Premio del quinto nivel: 900.000 pesos");
+	        System.out.println("¿ La batalla de boyaca ocurrió en ?");
+	        System.out.println("1: 1515");
+	        System.out.println("2: 1819.");
+	        System.out.println("3: 1810 ");
+	        System.out.println("4: 1924");
+
+	    };
+	    public void Pregunta4()
+	    {
+	        System.out.println("Premio del quinto nivel: 900.000 pesos");
+	        System.out.println("¿ Viaje al mundo en 80 dias fue escrito por: ?");
+	        System.out.println("1: Pablo Neruda  ");
+	        System.out.println("2: Edgar alan poe ");
+	        System.out.println("3: Julio verne.");
+	        System.out.println("4: Oscar Wilde ");
+
+	    };
+	    public void Pregunta5()
+	    {
+	        System.out.println("Premio del quinto nivel: 900.000 pesos");
+	        System.out.println("¿El primer mundial de futbol se disputó en   ?");
+	        System.out.println("1: Inglaterra");
+	        System.out.println("2: Brasil");
+	        System.out.println("3: Chile");
+	        System.out.println("4: uruguay");
+
+	    };
 
 }
