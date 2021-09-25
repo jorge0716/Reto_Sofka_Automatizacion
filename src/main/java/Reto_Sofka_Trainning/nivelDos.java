8df6ba6396a8e98e99b16163a7c5c75a6f2dd0f3
Jorge_Agudelo <williamchehezy>
2021-09-25 17:17:18 -0500

    Se implememnta la clase de las preguntas del segundo nivel

--- src/main/java/Reto_Sofka_Trainning/nivelDos.java
+++ src/main/java/Reto_Sofka_Trainning/nivelDos.java
@@ -1,5 +1,58 @@
 package Reto_Sofka_Trainning;
 
 public class nivelDos {
+	   public void Pregunta1()
+	    {
+	        System.out.println("Premio del segundo nivel: 400.000 pesos");
+	        System.out.println("¿Democracia es:?");
+	        System.out.println("1: Una ronda Infantil");
+	        System.out.println("2: Una comida tipica costeña ");
+	        System.out.println("3: Una forma de gobierno");
+	        System.out.println("4: Una sopa de letras");
+
+
+
+	    };
+	    public void Pregunta2()
+	    {
+	        System.out.println("Premio del segundo nivel: 400.000 pesos");
+	        System.out.println("¿ Una molecula es una agrupacion de?");
+	        System.out.println("1: Atomos");
+	        System.out.println("2: Numeros ");
+	        System.out.println("3: Ecuaciones ");
+	        System.out.println("4: Letras");
+
+	    };
+
+	    public void Pregunta3()
+	    {
+	        System.out.println("Premio del segundo nivel: 400.000 pesos");
+	        System.out.println("¿ Cual es la raiz cuadrada de 9?");
+	        System.out.println("1: 9");
+	        System.out.println("2: 8");
+	        System.out.println("3: 2");
+	        System.out.println("4: 3 ");
+
+	    };
+	    public void Pregunta4()
+	    {
+	        System.out.println("Premio del segundo nivel: 400.000 pesos");
+	        System.out.println("¿El sol es?");
+	        System.out.println("1: Un Asteroide ");
+	        System.out.println("2: Estrella ");
+	        System.out.println("3: Un cometa");
+	        System.out.println("4: Letras ");
+
+	    };
+	    public void Pregunta5()
+	    {
+	        System.out.println("Premio del segundo nivel: 400.000 pesos");
+	        System.out.println("¿Que artefacto se usa para salir a espacio?");
+	        System.out.println("1: Un velero");
+	        System.out.println("2: Un camion");
+	        System.out.println("3: Un cohete");
+	        System.out.println("4: Barco ");
+
+	    };
 
 }
